210ae194656316a718bc569f4c2318c3b81f4def
Stone Zhong <shidong.zhong@oracle.com>
2019-06-16 08:48:12 -0700

    change stage to quiet mode

--- mordor/bin/install_packages.sh
+++ mordor/bin/install_packages.sh
@@ -5,13 +5,13 @@
 # $3: version
 
 source $1/venvs/$2-$3/bin/activate
-pip install --upgrade pip
+pip -q install --upgrade pip
 if [ -e $1/apps/$2/$3/requirements_pre.txt ]
 then
-    pip install -r $1/apps/$2/$3/requirements_pre.txt
+    pip -q install -r $1/apps/$2/$3/requirements_pre.txt
 fi
 if [ -e $1/apps/$2/$3/requirements.txt ]
 then
-    pip install -r $1/apps/$2/$3/requirements.txt
+    pip -q install -r $1/apps/$2/$3/requirements.txt
 fi
 deactivate
--- mordor/mordor.py
+++ mordor/mordor.py
@@ -2,26 +2,27 @@
 # -*- coding: UTF-8 -*-
 
 from __future__ import print_function
-import argparse 
+import argparse
 import subprocess
 import os
 import json
 import tempfile
 import glob
 
+
 class HostConfig(object):
     def __init__(self, host_name, host_config):
         self.host_config = host_config
         self.name = host_name
-    
+
     @property
     def env_home(self):
         return self.host_config["env_home"]
-    
+
     @property
     def ssh_host(self):
         return self.host_config["ssh_host"]
-        
+
     @property
     def virtualenv(self):
         return self.host_config["virtualenv"]
@@ -29,7 +30,7 @@ class HostConfig(object):
     @property
     def python3(self):
         return self.host_config.get("python3")
-    
+
     @property
     def ssh_key_filename(self):
         v = self.host_config.get("ssh_key_filename")
@@ -43,13 +44,13 @@ class HostConfig(object):
 
     def path(self, *args):
         return os.path.join(self.env_home, *args)
-            
+
     def execute(self, *args):
-        if self.ssh_key_filename:        
+        if self.ssh_key_filename:
             new_args = [
-                "ssh", 
-                "-i", 
-                self.ssh_key_filename, 
+                "ssh",
+                "-i",
+                self.ssh_key_filename,
                 "{}@{}".format(self.ssh_username, self.ssh_host)
             ]
         else:
@@ -59,11 +60,11 @@ class HostConfig(object):
 
     # execute, wait for finish and get the output
     def execute2(self, *args):
-        if self.ssh_key_filename:        
+        if self.ssh_key_filename:
             new_args = [
-                "ssh", 
-                "-i", 
-                self.ssh_key_filename, 
+                "ssh",
+                "-i",
+                self.ssh_key_filename,
                 "{}@{}".format(self.ssh_username, self.ssh_host)
             ]
         else:
@@ -75,9 +76,9 @@ class HostConfig(object):
         return (output, err)
 
     def upload(self, local_path, remote_path):
-        if self.ssh_key_filename:        
+        if self.ssh_key_filename:
             new_args = [
-                "scp", "-i", self.ssh_key_filename,
+                "scp", "-q", "-i", self.ssh_key_filename,
                 local_path,
                 "{}@{}:{}".format(
                     self.ssh_username,
@@ -86,23 +87,24 @@ class HostConfig(object):
             ]
         else:
             new_args = [
-                "scp",
+                "scp", "-q",
                 local_path,
                 "{}:{}".format(self.ssh_host, remote_path)
             ]
 
         subprocess.call(new_args)
-    
+
+
 class AppConfig(object):
     def __init__(self, name, app_config):
         self.app_config = app_config
         self.name = name
         self.manifest = AppManifest(get_json(self.path("manifest.json")))
-    
+
     @property
     def home_dir(self):
         return self.app_config["home_dir"]
-    
+
     @property
     def cmd(self):
         return self.app_config.get("cmd", "run.sh")
@@ -115,27 +117,27 @@ class AppConfig(object):
     def config(self):
         # config files need to copied over
         return self.app_config.get("config", {})
-    
+
     @property
     def deploy_to(self):
         return self.app_config["deploy_to"]
 
     def path(self, *args):
         return os.path.join(self.home_dir, *args)
-    
+
     @property
     def archive_filename(self):
         return "{}-{}.tar.gz".format(self.name, self.manifest.version)
-    
+
     @property
     def venv_name(self):
         return "{}-{}".format(self.name, self.manifest.version)
-    
+
     def create_archive(self):
         temp_dir = tempfile.mkdtemp()
         args = [
-            'tar', 
-            '-czf', 
+            'tar',
+            '-czf',
             os.path.join(temp_dir, self.archive_filename),
             "-C",
             self.home_dir
@@ -156,24 +158,27 @@ class Config(object):
         self.app_dict = {}
         for (app_name, app_config) in self.config["applications"].items():
             self.app_dict[app_name] = AppConfig(app_name, app_config)
-    
+
     def get_host(self, host_name):
         return self.host_dict[host_name]
-    
+
     def get_app(self, app_name):
         return self.app_dict[app_name]
+
 
 def get_json(path):
     with open(os.path.expanduser(path), "r") as f:
         return json.load(f)
 
+
 class AppManifest(object):
     def __init__(self, manifest):
         self.manifest = manifest
-    
+
     @property
     def version(self):
         return self.manifest["version"]
+
 
 # Initialize a given server so it can run python code
 def init_host(base_dir, config, host_name):
@@ -216,33 +221,34 @@ def stage_app(base_dir, config, app_name, update_venv):
     for host_name in app.deploy_to:
         host = config.get_host(host_name)
         stage_app_on_host(base_dir, config, app, host, archive_filename, update_venv)
-    
+
+
 def stage_app_on_host(base_dir, config, app, host, archive_filename, update_venv):
     print("stage application \"{}\" on host \"{}\"".format(app.name, host.name))
     # copy app archive to remote host
     host.upload(archive_filename, host.path("temp", app.archive_filename))
 
     # create directorys
-    host.execute("mkdir", "-p" , host.path("apps", app.name))
-    host.execute("mkdir", "-p" , host.path("apps", app.name, app.manifest.version))
-    host.execute("rm",    "-rf", host.path("apps", app.name, app.manifest.version, "*"))
-    host.execute("mkdir", "-p" , host.path("logs", app.name))
-    host.execute("mkdir", "-p" , host.path("configs", app.name))
-    host.execute("mkdir", "-p" , host.path("data", app.name))
+    host.execute("mkdir", "-p", host.path("apps", app.name))
+    host.execute("mkdir", "-p", host.path("apps", app.name, app.manifest.version))
+    host.execute("rm", "-rf", host.path("apps", app.name, app.manifest.version, "*"))
+    host.execute("mkdir", "-p", host.path("logs", app.name))
+    host.execute("mkdir", "-p", host.path("configs", app.name))
+    host.execute("mkdir", "-p", host.path("data", app.name))
 
     # remove and re-create the sym link point to the current version of the app
-    host.execute("rm",    "-f",  host.path("apps", app.name, "current"))
+    host.execute("rm", "-f", host.path("apps", app.name, "current"))
     host.execute(
-        "ln", 
-        "-s", 
+        "ln",
+        "-s",
         host.path("apps", app.name, app.manifest.version),
         host.path("apps", app.name, "current")
     )
 
     # extract app archive
     host.execute(
-        'tar', 
-        '-xzf', 
+        'tar',
+        '-xzf',
         host.path("temp", app.archive_filename),
         "-C",
         host.path("apps", app.name, app.manifest.version)
@@ -256,23 +262,23 @@ def stage_app_on_host(base_dir, config, app, host, archive_filename, update_venv
         else:
             host.execute(host.virtualenv, host.path("venvs", app.venv_name))
         host.execute(
-            host.path("bin", "install_packages.sh"), 
+            host.path("bin", "install_packages.sh"),
             host.env_home,
             app.name,
             app.manifest.version
         )
         # create a symlink
         host.execute("rm", "-f", host.path("venvs", app.name))
-        host.execute("ln", "-s", 
-            host.path("venvs", app.venv_name),
-            host.path("venvs", app.name)
-        )
+        host.execute("ln", "-s",
+                     host.path("venvs", app.venv_name),
+                     host.path("venvs", app.name)
+                     )
 
     for (filename, deploy_type) in app.config.items():
         if deploy_type == "copy":
             host.upload(
                 os.path.join(
-                    os.path.expanduser("~/.mordor/configs"), 
+                    os.path.expanduser("~/.mordor/configs"),
                     app.name,
                     filename
                 ),
@@ -281,22 +287,23 @@ def stage_app_on_host(base_dir, config, app, host, archive_filename, update_venv
             continue
         if deploy_type == "convert":
             with open(
-                os.path.join(os.path.expanduser("~/.mordor/configs"), app.name, filename),
-                "r"
+                    os.path.join(os.path.expanduser("~/.mordor/configs"), app.name, filename),
+                    "r"
             ) as f:
                 content = f.read()
             config_dir = os.path.join(host.env_home, "configs", app.name)
             content = content.format(
-                config_dir = config_dir,
-                env_home = host.env_home,
-                app_name = app.name
+                config_dir=config_dir,
+                env_home=host.env_home,
+                app_name=app.name
             )
-            tf = tempfile.NamedTemporaryFile(delete = False)
+            tf = tempfile.NamedTemporaryFile(delete=False)
             tf.file.write(content.encode("utf-8"))
             tf.file.close()
             host.upload(tf.name, host.path("configs", app.name, filename))
             continue
     return
+
 
 def run_app(base_dir, config, app_name):
     app = config.get_app(app_name)
@@ -305,13 +312,14 @@ def run_app(base_dir, config, app_name):
         host = config.get_host(host_name)
         run_app_on_host(base_dir, config, app, host)
 
+
 def run_app_on_host(base_dir, config, app, host):
     print("    {}".format(host.name))
     if app.cmd.endswith(".sh"):
         host.execute(
             host.path("bin", "run_app.sh"),
             host.env_home,
-            app.name, 
+            app.name,
             app.cmd
         )
         return
@@ -333,6 +341,7 @@ def kill_app(base_dir, config, app_name):
         host = config.get_host(host_name)
         kill_app_on_host(base_dir, config, app, host)
 
+
 def kill_app_on_host(base_dir, config, app, host):
     print("    {}".format(host.name))
     host.execute(
@@ -341,12 +350,14 @@ def kill_app_on_host(base_dir, config, app, host):
         app.name
     )
 
+
 def get_app_status(base_dir, config, app_name):
     app = config.get_app(app_name)
     print("status of application \"{}\"".format(app.name))
     for host_name in app.deploy_to:
         host = config.get_host(host_name)
         get_app_status_on_host(base_dir, config, app, host)
+
 
 def get_app_status_on_host(base_dir, config, app, host):
     output, err = host.execute2(
@@ -355,6 +366,7 @@ def get_app_status_on_host(base_dir, config, app, host):
         app.name
     )
     print('    {}: {}'.format(host.name, output.decode('utf-8')))
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -370,10 +382,10 @@ def main():
         "-p", "--app-name", type=str, required=False, help="application name"
     )
     parser.add_argument(
-        "--update-venv",  
-        type=str, 
-        required=False, 
-        default="T", 
+        "--update-venv",
+        type=str,
+        required=False,
+        default="T",
         help="Should I update virtual env?"
     )
     args = parser.parse_args()
@@ -389,14 +401,14 @@ def main():
             return
         init_host(base_dir, config, args.host_name)
         return
-    
-    if args.action =="stage":
+
+    if args.action == "stage":
         if not args.app_name:
             print("--app-name must be specified")
             return
         stage_app(base_dir, config, args.app_name, args.update_venv == 'T')
         return
-    
+
     if args.action == "run":
         run_app(base_dir, config, args.app_name)
         return
@@ -410,6 +422,7 @@ def main():
         return
 
     raise Exception("unrecognized action")
+
 
 if __name__ == '__main__':
     main()
