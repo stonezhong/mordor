a5895c56c7fe04b8b01b6140f39756dadc1f203d
Stone Zhong <stonezhong@hotmail.com>
2021-01-01 19:18:06 -0800

    fix run_dispatch

--- mordor/bin/run_dispatcher.sh
+++ mordor/bin/run_dispatcher.sh
@@ -3,9 +3,8 @@
 # $1: mordor home dir
 # $2: app name
 # $3: cmd
-# $4: cmd-options
 
 source $1/venvs/$2/bin/activate
 cd $1/apps/$2/current
 
-python -u dispatch.py $(echo $4|base64 --decode)
+python -u dispatch.py $(echo $3|base64 --decode)
