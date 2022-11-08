49d58c1c11fbc47832bc146595d3eb1ebf58f4a1
Stone Zhong <stonezhong@hotmail.com>
2022-11-08 02:59:44 -0800

    improve AppEnv class and improcess docker examples

--- samples/docker/src/dispatch.py
+++ samples/docker/src/dispatch.py
@@ -19,6 +19,13 @@ def main():
 
     app_env = AppEnv("sample")
     context = app_env.get_config("_deployment.json")
+    docker = app_env.get_config("docker.json")
+    docker_app_env = AppEnv(app_env.app_name, version=app_env.version, env_home=docker["env_home"])
+    context.update({
+        "docker": docker,
+        "docker_app_env": docker_app_env,
+        "app_env": app_env
+    })
     prepare_for_docker("sample", context)
 
 if __name__ == '__main__':
--- samples/docker/src/docker/context.py
+++ samples/docker/src/docker/context.py
@@ -1,10 +1,11 @@
 from copy import deepcopy
 
 def get_context(context):
-    context.update({
-        "user_name": "stonezhong",
-        "user_id": 1000,
-        "group_name": "stonezhong",
-        "group_id": 1000
-    })
+    # You can update context here if needed
+    # context.update({
+    #     "user_name": "stonezhong",
+    #     "user_id": 1000,
+    #     "group_name": "stonezhong",
+    #     "group_id": 1000
+    # })
     return context
